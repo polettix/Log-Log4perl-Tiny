5328c7a2bf5e051e3350a637d87a38d4aedbbfaf
Flavio Poletti <flavio@polettix.it>
2010-07-26 16:57:04 +0200

    little enhacements, documentation

--- lib/Log/Log4perl/Tiny.pm
+++ lib/Log/Log4perl/Tiny.pm
@@ -44,22 +44,24 @@ sub import {
          );
       } ## end elsif ($item eq ':subs')
       elsif ($item =~ /\A : (mimic | mask | fake) \z/mxs) {
-         $INC{'Log/Log4perl.pm'} = __FILE__;
-         *Log::Log4perl::import = sub { };
-         *Log::Log4perl::easy_init = sub {
-            my ($pack, $conf) = @_;
-            $_instance = __PACKAGE__->new($conf) if ref $conf;
-            if (ref $conf) {
-               $_instance->level($conf->{level}) if exists $conf->{level};
-               $_instance->format($conf->{format})
-                 if exists $conf->{format};
-               $_instance->format($conf->{layout})
-                 if exists $conf->{layout};
-            } ## end if (ref $conf)
-            elsif (defined $conf) {
-               $_instance->level($conf);
-            }
-         };
+         if (! Log::Log4perl->can('easy_init')) {
+            $INC{'Log/Log4perl.pm'} = __FILE__;
+            *Log::Log4perl::import = sub { };
+            *Log::Log4perl::easy_init = sub {
+               my ($pack, $conf) = @_;
+               $_instance = __PACKAGE__->new($conf) if ref $conf;
+               if (ref $conf) {
+                  $_instance->level($conf->{level}) if exists $conf->{level};
+                  $_instance->format($conf->{format})
+                  if exists $conf->{format};
+                  $_instance->format($conf->{layout})
+                  if exists $conf->{layout};
+               } ## end if (ref $conf)
+               elsif (defined $conf) {
+                  $_instance->level($conf);
+               }
+            };
+         }
       } ## end elsif ($item =~ /\A : (mimic | mask | fake) \z/mxs)
       elsif ($item eq ':easy') {
          push @list, qw( :levels :subs :fake );
@@ -78,6 +80,12 @@ sub new {
    if (exists $args{file}) {
       open my $fh, $args{file}
         or die "open('$args{file}'): $!";
+
+      # Autoflush opened file
+      my $previous = select($fh);
+      $|++;
+      select($previous);
+
       $args{fh} = $fh;
    }
 
@@ -96,9 +104,8 @@ sub new {
    return $self;
 } ## end sub new
 
-sub get_logger {
-   return $_instance ||= __PACKAGE__->new();
-}
+sub get_logger { return $_instance ||= __PACKAGE__->new(); }
+sub LOGLEVEL { return get_logger()->level(@_); }
 
 sub format {
    my $self = shift;
@@ -130,6 +137,8 @@ sub format {
    return $self->{format};
 } ## end sub format
 
+*layout = \&format;
+
 sub log {
    my $self = shift;
 
@@ -151,6 +160,7 @@ sub ALWAYS { return $_instance->log($OFF, @_); }
 sub _exit {
    my $self = shift || $_instance;
    exit $self->{logexit_code} if defined $self->{logexit_code};
+   exit $Log::Log4perl::LOGEXIT_CODE if defined $Log::Log4perl::LOGEXIT_CODE;
    exit 1;
 }
 
@@ -282,7 +292,7 @@ BEGIN {
       };
    } ## end for my $name (qw( FATAL ERROR WARN INFO DEBUG TRACE...
 
-   for my $accessor (qw( level fh )) {
+   for my $accessor (qw( level fh logexit_code )) {
       *{__PACKAGE__ . '::' . $accessor} = sub {
          my $self = shift;
          $self = $_instance unless ref $self;
@@ -304,41 +314,519 @@ __END__
 
 =head1 NAME
 
-Log::Log4perl::Tiny - [Una riga di descrizione dello scopo del modulo]
+Log::Log4perl::Tiny - mimic Log::Log4perl in one single module
 
 =head1 VERSION
 
-This document describes Log::Log4perl::Tiny version 0.0.1. Most likely, this
-version number here is outdate, and you should peek the source.
-
+This document describes Log::Log4perl::Tiny version 0.1.
 
 =head1 SYNOPSIS
 
-   use Log::Log4perl::Tiny;
+   use Log::Log4perl::Tiny qw( :easy );
+   Log::Log4perl->easy_init({
+      file   => '>>/var/log/something.log',
+      layout => '[%d] [%-5P:%-5p] %m%n',
+      level  => $INFO,
+   });
 
-=for l'autore, da riempire:
-   Qualche breve esempio con codice che mostri l'utilizzo più comune.
-   Questa sezione sarà quella probabilmente più letta, perché molti
-   utenti si annoiano a leggere tutta la documentazione, per cui
-   è meglio essere il più educativi ed esplicativi possibile.
+   WARN 'something weird happened';
+   INFO 'just doing it';
+   DEBUG 'this does not get printed at $INFO level';
 
+   # LOGLEVEL isn't in Log::Log4perl, but might come handy
+   LOGLEVEL($DEBUG);   # enable debugging for small section
+   # otherwise, "get_logger()->level($DEBUG)", see below
+
+   DEBUG 'now this gets printed';
+   LOGLEVEL($INFO);    # disable debugging again
+   DEBUG 'skipped, again';
+   DEBUG 'complex evaluation value:', sub { 
+      # evaluation skipped if log level filters DEBUG out
+   };
+
+   # Object-oriented interface is available as well
+   my $logger = get_logger();
+   $logger->level($DEBUG);   # enable debugging for small section
+   $logger->debug('whatever you want');
+   $logger->level($INFO);    # disable debugging again
+
+   # All stealth loggers are available
+   LOGCONFESS 'I cannot accept this, for a whole stack of reasons!';
+
+   # Want to send the output somewhere else?
+   use IO::Handle;
+   open my $fh, '>>', '/path/to/new.log';
+   $fh->autoflush();
+   $logger->fh($fh);
+
+   # Change layout?
+   $logger->layout('[%d %p] %m%n');
+   # or, equivalently
+   $logger->format('[%d %p] %m%n');
 
 =head1 DESCRIPTION
 
-=for l'autore, da riempire:
-   Fornite una descrizione completa del modulo e delle sue caratteristiche.
-   Aiutatevi a strutturare il testo con le sottosezioni (=head2, =head3)
-   se necessario.
+Yes... yet another logging module. Nothing particularly fancy nor
+original, too, but a single-module implementation of the features I
+use most from L<Log::Log4perl> for quick things, namely:
+
+=over
+
+=item *
+
+easy mode and stealth loggers (aka log functions C<INFO>, C<WARN>, etc.);
+
+=item *
+
+debug message filtering by log level;
+
+=item *
+
+line formatting customisation;
+
+=item *
+
+quick sending of messages to a log file.
+
+=back
+
+There are many, many things that are not included; probably the most
+notable one is the ability to provide a configuration file.
+
+=head2 Why?
+
+I have really nothing against L<Log::Log4perl>, to the point that
+one of the import options is to check whether L<Log::Log4perl> is installed
+and use it if possible. I just needed to crunch the plethora of
+modules down to a single-file module, so that I can embed it easily in
+scripts I use in machines where I want to reduce my impact as much as
+possible.
+
+=head2 Log Levels
+
+L<Log::Log4perl::Tiny> implements all I<standard> L<Log::Log4perl>'s
+log levels, without the possibility to change them. The corrispondent
+values are available in the following variables (in order of increasing
+severity or I<importance>):
+
+=over
+
+=item C<< $TRACE >>
+
+=item C<< $DEBUG >>
+
+=item C<< $INFO >>
+
+=item C<< $WARN >>
+
+=item C<< $ERROR >>
+
+=item C<< $FATAL >>
+
+=back
+
+You can import these variables using the C<:levels> import facility,
+or you can use the directly from the L<Log::Log4perl::Tiny> namespace.
+They are imported automatically if the C<:easy> import option is specified.
+
+=head2 Easy Mode Overview
+
+I love L<Log::Log4perl>'s easy mode because it lets you set up a
+sophisticated logging infrastructure with just a few keystrokes:
+
+   use Log::Log4perl qw( :easy );
+   Log::Log4perl->easy_init({
+      file   => '>>/var/log/something.log',
+      layout => '[%d] [%-5P:%-5p] %m%n',
+      level  => $INFO,
+   });
+   INFO 'program started, yay!';
+
+   use Data::Dumper;
+   DEBUG 'Some stuff in main package', sub { Dumper(\%main::) };
+
+If you want, you can replicate it with just a change in the first line:
+
+   use Log::Log4perl::Tiny qw( :easy );
+   Log::Log4perl->easy_init({
+      file   => '>>/var/log/something.log',
+      layout => '[%d] [%-5P:%-5p] %m%n',
+      level  => $INFO,
+   });
+   INFO 'program started, yay!';
+
+   use Data::Dumper;
+   DEBUG 'Some stuff in main package', sub { Dumper(\%main::) };
+
+Well... yes, I'm invading the L<Log::Log4perl> namespace in order to
+reduce the needed changes as mush as possible. This is useful when I
+begin using L<Log::Log4perl> and then realise I want to make a single
+script with all modules embedded. There is also another reason why
+I put C<easy_init()> in L<Log::Log4perl> namespace:
+
+   use Log::Log4perl::Tiny qw( :full_or_fake :easy );
+   Log::Log4perl->easy_init({
+      file   => '>>/var/log/something.log',
+      layout => '[%d] [%-5P:%-5p] %m%n',
+      level  => $INFO,
+   });
+   INFO 'program started, yay!';
+
+   use Data::Dumper;
+   DEBUG 'Some stuff in main package', sub { Dumper(\%main::) };
+
+With import option C<full_or_fake>, in fact, the module first tries to
+load L<Log::Log4perl> in the caller's namespace with the provided
+options (except C<full_or_fake>, of course), returning immediately if
+it is successful; otherwise, it tries to "fake" L<Log::Log4perl> and
+installs its own logging functions. In this way, if L<Log::Log4perl>
+is available it will be used, but you don't have to change anything
+if it isn't.
+
+Easy mode tries to mimic what L<Log::Log4perl> does, or at least
+the things that (from a purely subjective point of view) are most
+useful: C<easy_init()> and I<stealth loggers>.
+
+=head2 C<easy_init()>
+
+L<Log::Log4perl::Tiny> only supports three options from the big
+brother:
+
+=over
+
+=item C<< level >>
+
+the log level threshold. Logs sent at a higher or equal priority
+(i.e. at a more I<important> level, or equal) will be printed out,
+the others will be ignored. The default value is C<$INFO>;
+
+=item C<< file >>
+
+a file name where to send the log lines. For compatibility with
+L<Log::Log4perl>, a 2-arguments C<open()> will be performed, which
+means you can easily set the opening mode, e.g. C<<< >>filename >>>.
+The default is to send logging messages to C<STDERR>;
+
+=item C<< layout >>
+
+the log line layout (it can also be spelled C<format>, they are
+synonims). The default value is the following:
+
+   [%d] [%5p] %m%n
+
+which means I<date in brackets, then log level in brackets always
+using five chars, left-aligned, the log message and a newline>.
+
+=back
+
+If you call C<easy_init()> with a single unblessed scalar, it is
+considered to be the C<level> and it will be set accordingly.
+Otherwise, you have to pass a hash ref with the keys above.
+
+
+=head2 Stealth Loggers
+
+Stealth loggers are functions that emit a log message at a given
+severity; they are installed when C<:easy> mode is turned on.
+
+They are named after the corresponding level:
+
+
+=over
+
+=item C<< TRACE >>
+
+=item C<< DEBUG >>
+
+=item C<< INFO >>
+
+=item C<< WARN >>
+
+=item C<< ERROR >>
+
+=item C<< FATAL >>
+
+=back
+
+Additionally, you get the following logger functions (again, these are
+in line with L<Log::Log4perl>):
+
+=over
+
+=item C<< ALWAYS >>
+
+emit log whatever the configured logging level;
+
+=item C<< LOGWARN >>
+
+emit log at C<WARN> level, C<warn()> and then exit;
+
+=item C<< LOGDIE >>
+
+emit log at C<FATAL> level, C<die()> and then exit (if C<die()>
+didn't already exit);
+
+=item C<< LOGEXIT >>
+
+emit log at C<FATAL> level and then exit;
+
+=item C<< LOGCARP >>
+
+emit log at C<WARN> level and then call C<Carp::carp()>;
+
+=item C<< LOGCLUCK >>
+
+emit log at C<WARN> level and then call C<Carp::cluck()>;
+
+=item C<< LOGCROAK >>
+
+emit log at C<FATAL> level and then call C<Carp::croak()>;
+
+=item C<< LOGCONFESS >>
+
+emit log at C<FATAL> level and then call C<Carp::confess()>;
+
+=back
+
+If you want to set the exit code for C<LOGWARN> and C<LOGEXIT> above
+(and L<LOGDIE> as well, in case c<die()> does not exit by itself),
+you can go "the L<Log::Log4perl> way" and set
+C<$Log::Log4perl::LOGEXIT_CODE>, or set a code with
+C<logexit_code()> - but you have to wait to read something about the
+object-oriented interface before doing this!
+
+There is also one additional stealth function that L<Log::Log4perl>
+misses but that I think is of the outmoste importance: L<LOGLEVEL>, to
+set the log level threshold for printing. If you want to be 100%
+compatible with Log::Log4perl, anyway, you should rather do the following:
+
+   get_logger()->level(...);  # instead of LOGLEVEL(...)
+
+=head2 Emitting Logs
+
+To emit a log, you can call any of the stealth logger functions or any
+of the corresponding log methods. All the parameters that you pass are
+sent to the output stream as the are, except code references that are
+first evaluated. This lets you embed costly evaluations (e.g. generate
+heavy dumps of variabls) inside subroutines, and avoid the cost
+of evaluation in case the log is filtered out:
+
+   use Data::Dumper;
+   LOGLEVEL($INFO); # cut DEBUG and TRACE out
+   TRACE 'costly evaluation: ', sub { Dumper($heavy_object) };
+   # Dumper() is not actually called because DEBUG level is
+   # filtered out
+
+If you use the C<log()> method, the first parameter is the log level,
+then the others are interpreted as described above.
+
+=head2 Log Line Layout
+
+The log line layout sets the contents of a log line. The layout is
+configured as a C<printf>-like string, with placeholder identifiers
+that are modeled (with simplifications) after L<Log::Log4perl>'s ones:
+
+
+    %c Category of the logging event.
+    %C Fully qualified package (or class) name of the caller
+    %d Current date in yyyy/MM/dd hh:mm:ss format
+    %F File where the logging event occurred
+    %H Hostname
+    %l Fully qualified name of the calling method followed by the
+       callers source the file name and line number between 
+       parentheses.
+    %L Line number within the file where the log statement was issued
+    %m The message to be logged
+    %M Method or function where the logging request was issued
+    %n Newline (OS-independent)
+    %p Priority of the logging event
+    %P pid of the current process
+    %r Number of milliseconds elapsed from program start to logging 
+       event
+    %% A literal percent (%) sign
+
+Notably, both C<%x> (NDC) and C<%X> (MDC) are missing. Moreover, the
+extended specifier feature with additional info in braces (like
+C<%d{HH:mm}> is missing, i.e. the structure of each specifier above
+is fixed. (Thanks to C<Log::Tiny> for the cool trick of how to handle
+the C<printf>-like string, which is probably mutuated from
+C<Log::Log4perl> itself according to the comments).
 
 
 =head1 INTERFACE 
 
-=for l'autore, da riempire:
-   Scrivete una sezione separata che elenchi i componenti pubblici
-   dell'interfaccia del modulo. Questi normalmente sono formati o
-   dalle subroutine che possono essere esportate, o dai metodi che
-   possono essere chiamati su oggetti che appartengono alle classi
-   fornite da questo modulo.
+You have two interfaces at your disposal, the functional one (with all
+the stealth logger functions) and the object-oriented one (with
+explicit actions upon a logger object). Choose your preferred option.
+
+=head2 Functional Interface
+
+The functional interface sports the following functions (imported
+automatically when C<:easy> is passed as import option):
+
+=over
+
+=item C<< TRACE >>
+
+=item C<< DEBUG >>
+
+=item C<< INFO >>
+
+=item C<< WARN >>
+
+=item C<< ERROR >>
+
+=item C<< FATAL >>
+
+stealth logger functions, each emits a log at the corresponding level;
+
+=item C<< ALWAYS >>
+
+emit log whatever the configured logging level;
+
+=item C<< LOGWARN >>
+
+emit log at C<WARN> level, C<warn()> and then exit;
+
+=item C<< LOGDIE >>
+
+emit log at C<FATAL> level, C<die()> and then exit (if C<die()>
+didn't already exit);
+
+=item C<< LOGEXIT >>
+
+emit log at C<FATAL> level and then exit;
+
+=item C<< LOGCARP >>
+
+emit log at C<WARN> level and then call C<Carp::carp()>;
+
+=item C<< LOGCLUCK >>
+
+emit log at C<WARN> level and then call C<Carp::cluck()>;
+
+=item C<< LOGCROAK >>
+
+emit log at C<FATAL> level and then call C<Carp::croak()>;
+
+=item C<< LOGCONFESS >>
+
+emit log at C<FATAL> level and then call C<Carp::confess()>;
+
+=item C<< LOGLEVEL >> (not in L<Log::Log4perl>)
+
+set the minimum log level for sending a log message to the output;
+
+=back
+
+=head2 Object-Oriented Interface
+
+The functional interface is actually based upon actions on
+a pre-defined fixed instance of a C<Log::Log4perl::Tiny> object,
+so you can do the same with a logger object as well:
+
+=over
+
+=item C<< get_logger >>
+
+this function gives you the pre-defined logger instance (i.e. the
+same used by the stealth logger functions described above).
+
+=item C<< new >>
+
+if for obscure reasons the default logger isn't what you want, you
+can get a brand new object!
+
+=back
+
+The methods you can call upon the object mimic the functional
+interface, but with lowercase method names:
+
+=over
+
+=item C<< trace >>
+
+=item C<< debug >>
+
+=item C<< info >>
+
+=item C<< warn >>
+
+=item C<< error >>
+
+=item C<< fatal >>
+
+stealth logger functions, each emits a log at the corresponding level;
+
+=item C<< always >>
+
+emit log whatever the configured logging level;
+
+=item C<< logwarn >>
+
+emit log at C<WARN> level, C<warn()> and then exit;
+
+=item C<< logdie >>
+
+emit log at C<FATAL> level, C<die()> and then exit (if C<die()>
+didn't already exit);
+
+=item C<< logexit >>
+
+emit log at C<FATAL> level and then exit;
+
+=item C<< logcarp >>
+
+emit log at C<WARN> level and then call C<Carp::carp()>;
+
+=item C<< logcluck >>
+
+emit log at C<WARN> level and then call C<Carp::cluck()>;
+
+=item C<< logcroak >>
+
+emit log at C<FATAL> level and then call C<Carp::croak()>;
+
+=item C<< logconfess >>
+
+emit log at C<FATAL> level and then call C<Carp::confess()>;
+
+=back
+
+The main logging function is actually the following:
+
+=over
+
+=item C<< log >>
+
+the first parameter is the log level, the rest is the message to log
+
+=back
+
+Additionally, you have the following accessors:
+
+=over
+
+=item C<< level >>
+
+set the minimum level for sending messages to the output stream;
+
+=item C<< fh >>
+
+set the output filehandle;
+
+=item C<< format >>
+
+=item C<< layout >>
+
+set the line formatting;
+
+=item C<< logexit_code >>
+
+set the exit code to be used with C<logexit()> and C<logwarn()> (and
+C<logdie()> as well if C<die()> doesn't exit).
+
+=back
 
 
 =head1 DIAGNOSTICS
