45cdcafd443e66a2e93c47632863c0a4eeb9637d
Flavio Poletti <flavio@polettix.it>
2010-12-04 18:23:43 +0100

    cleaned-up documentation to let Dist::Zilla do its work

--- lib/Log/Log4perl/Tiny.pm
+++ lib/Log/Log4perl/Tiny.pm
@@ -1,6 +1,5 @@
 package Log::Log4perl::Tiny;
-
-our $VERSION = '0.1';
+# ABSTRACT: mimic Log::Log4perl in one single module
 
 use warnings;
 use strict;
@@ -330,14 +329,6 @@ sub level {
 
 1;                  # Magic true value required at end of module
 __END__
-
-=head1 NAME
-
-Log::Log4perl::Tiny - mimic Log::Log4perl in one single module
-
-=head1 VERSION
-
-This document describes Log::Log4perl::Tiny version 0.1.
 
 =head1 SYNOPSIS
 
@@ -848,168 +839,20 @@ C<logdie()> as well if C<die()> doesn't exit).
 =back
 
 
-=head1 DIAGNOSTICS
-
-=for l'autore, da riempire:
-   Elencate qualunque singolo errore o messaggio di avvertimento che
-   il modulo può generare, anche quelli che non "accadranno mai".
-   Includete anche una spiegazione completa di ciascuno di questi
-   problemi, una o più possibili cause e qualunque rimedio
-   suggerito.
-
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Descrizione di un errore]
-
-=item C<< Another error message here >>
-
-[Descrizione di un errore]
-
-[E così via...]
-
-=back
-
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=for l'autore, da riempire:
-   Una spiegazione completa di qualunque sistema di configurazione
-   utilizzato dal modulo, inclusi i nomi e le posizioni dei file di
-   configurazione, il significato di ciascuna variabile di ambiente
-   utilizzata e proprietà che può essere impostata. Queste descrizioni
-   devono anche includere dettagli su eventuali linguaggi di configurazione
-   utilizzati.
-  
-Log::Log4perl::Tiny requires no configuration files or environment variables.
-
-
 =head1 DEPENDENCIES
-
-=for l'autore, da riempire:
-   Una lista di tutti gli altri moduli su cui si basa questo modulo,
-   incluse eventuali restrizioni sulle relative versioni, ed una
-   indicazione se il modulo in questione è parte della distribuzione
-   standard di Perl, parte della distribuzione del modulo o se
-   deve essere installato separatamente.
 
 None.
 
 
-=head1 INCOMPATIBILITIES
-
-=for l'autore, da riempire:
-   Una lista di ciascun modulo che non può essere utilizzato
-   congiuntamente a questo modulo. Questa condizione può verificarsi
-   a causa di conflitti nei nomi nell'interfaccia, o per concorrenza
-   nell'utilizzo delle risorse di sistema o di programma, o ancora
-   a causa di limitazioni interne di Perl (ad esempio, molti dei
-   moduli che utilizzano filtri al codice sorgente sono mutuamente
-   incompatibili).
-
-None reported.
-
-
 =head1 BUGS AND LIMITATIONS
-
-=for l'autore, da riempire:
-   Una lista di tutti i problemi conosciuti relativi al modulo,
-   insime a qualche indicazione sul fatto che tali problemi siano
-   plausibilmente risolti in una versione successiva. Includete anche
-   una lista delle restrizioni sulle funzionalità fornite dal
-   modulo: tipi di dati che non si è in grado di gestire, problematiche
-   relative all'efficienza e le circostanze nelle quali queste possono
-   sorgere, limitazioni pratiche sugli insiemi dei dati, casi
-   particolari che non sono (ancora) gestiti, e così via.
 
 No bugs have been reported.
 
 Please report any bugs or feature requests through http://rt.cpan.org/
 
 
-=head1 AUTHOR
-
-Flavio Poletti  C<< <flavio [at] polettix [dot] it> >>
-
-
-=head1 LICENCE AND COPYRIGHT
-
-Copyright (c) 2010, Flavio Poletti C<< <flavio [at] polettix [dot] it> >>. All rights reserved.
-
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl 5.8.x itself. See L<perlartistic>
-and L<perlgpl>.
-
-Questo modulo è software libero: potete ridistribuirlo e/o
-modificarlo negli stessi termini di Perl 5.8.x stesso. Vedete anche
-L<perlartistic> e L<perlgpl>.
-
-
-=head1 DISCLAIMER OF WARRANTY
-
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
-
-=head1 NEGAZIONE DELLA GARANZIA
-
-Poiché questo software viene dato con una licenza gratuita, non
-c'è alcuna garanzia associata ad esso, ai fini e per quanto permesso
-dalle leggi applicabili. A meno di quanto possa essere specificato
-altrove, il proprietario e detentore del copyright fornisce questo
-software "così com'è" senza garanzia di alcun tipo, sia essa espressa
-o implicita, includendo fra l'altro (senza però limitarsi a questo)
-eventuali garanzie implicite di commerciabilità e adeguatezza per
-uno scopo particolare. L'intero rischio riguardo alla qualità ed
-alle prestazioni di questo software rimane a voi. Se il software
-dovesse dimostrarsi difettoso, vi assumete tutte le responsabilità
-ed i costi per tutti i necessari servizi, riparazioni o correzioni.
-
-In nessun caso, a meno che ciò non sia richiesto dalle leggi vigenti
-o sia regolato da un accordo scritto, alcuno dei detentori del diritto
-di copyright, o qualunque altra parte che possa modificare, o redistribuire
-questo software così come consentito dalla licenza di cui sopra, potrà
-essere considerato responsabile nei vostri confronti per danni, ivi
-inclusi danni generali, speciali, incidentali o conseguenziali, derivanti
-dall'utilizzo o dall'incapacità di utilizzo di questo software. Ciò
-include, a puro titolo di esempio e senza limitarsi ad essi, la perdita
-di dati, l'alterazione involontaria o indesiderata di dati, le perdite
-sostenute da voi o da terze parti o un fallimento del software ad
-operare con un qualsivoglia altro software. Tale negazione di garanzia
-rimane in essere anche se i dententori del copyright, o qualsiasi altra
-parte, è stata avvisata della possibilità di tali danneggiamenti.
-
-Se decidete di utilizzare questo software, lo fate a vostro rischio
-e pericolo. Se pensate che i termini di questa negazione di garanzia
-non si confacciano alle vostre esigenze, o al vostro modo di
-considerare un software, o ancora al modo in cui avete sempre trattato
-software di terze parti, non usatelo. Se lo usate, accettate espressamente
-questa negazione di garanzia e la piena responsabilità per qualsiasi
-tipo di danno, di qualsiasi natura, possa derivarne.
-
 =head1 SEE ALSO
 
-=for l'autore, da riempire:
-   Una lista di moduli/link da considerare per completare le funzionalità
-   del modulo, o per trovarne di alternative.
+L<Log::Log4perl> is one of the most useful modules I ever used, go check it!
 
 =cut
