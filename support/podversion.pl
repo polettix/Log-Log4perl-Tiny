27c1221d4a9cfd2ed061484b4f4b8335938c1ee2
Flavio Poletti <flavio@polettix.it>
2016-05-29 11:44:24 +0200

    added script for updating version in docs

---
+++ support/podversion.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Template::Perlish ();
+use Path::Tiny;
+use 5.010;
+
+my ($distro, $version) = @ARGV;
+my $podfile =
+  path($distro)->child(qw< lib Log Log4perl Tiny.pod >);
+my $readme = path($distro)->child('README');
+
+my $tp = Template::Perlish->new(
+   start     => '{{[',
+   stop      => ']}}',
+   variables => {
+      distro  => $distro,
+      version => $version,
+   },
+);
+
+for my $file ($podfile, $readme) {
+   my $rendered = $tp->process($file->slurp_raw());
+   $file->spew_raw($rendered);
+}
